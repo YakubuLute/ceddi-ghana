c86189bac29b66d4aefdb1e52560526e1908a0c2
Yakubu Lute <yakubulute@outlook.com>
2025-07-05 06:37:12 +0000

    chore:integrated components into the home components

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { AtAGlanceComponent } from './at-a-glance/at-a-glance.component';
+import { ImpactCounterComponent } from './impact-counter/impact-counter.component';
+import { UpcomingEventsComponent } from './upcoming-events/upcoming-events.component';
+import { SuccessStoryComponent } from './success-story/success-story.component';
+import { NewsletterSectionComponent } from './newsletter-section/newsletter-section.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    HeroSectionComponent,
+    AtAGlanceComponent,
+    ImpactCounterComponent,
+    UpcomingEventsComponent,
+    SuccessStoryComponent,
+    NewsletterSectionComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
