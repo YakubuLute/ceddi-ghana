3f2b705ca96aa6fa88f42b9d3987b1544cb6c312
Yakubu Lute <yakubulute@outlook.com>
2025-07-07 10:34:31 +0000

    feature: integrated homepage

--- ceddi-ghana-website/src/app/pages/home/home.component.ts
+++ ceddi-ghana-website/src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [MatCardModule],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true
 })
 export class HomeComponent {
-
+  focus: boolean = false;
+  focus1: boolean = false;
+  
 }
